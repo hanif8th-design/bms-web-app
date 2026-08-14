e1ee1d43b7d07c1be8414972cea139cf70e2f467
Ihsanullah Khan <ihsanuk.official@gmail.com>
2026-08-15 02:19:23 +0500

    create a demo homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { HomePage } from './pages/HomePage/HomePage'
 import { LoginPage } from './pages/LoginPage/LoginPage'
 import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage'
 import { RegisterPage } from './pages/RegisterPage/RegisterPage'
@@ -6,6 +7,7 @@ import { RegisterPage } from './pages/RegisterPage/RegisterPage'
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="*" element={<NotFoundPage />} />
---
+++ client/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { PublicNavbar } from '../../components/PublicNavbar/PublicNavbar'
+
+export function HomePage() {
+  return (
+    <>
+      <PublicNavbar />
+      <main>
+        <h1>home page</h1>
+      </main>
+    </>
+  )
+}
